486d0af923911f92f6d2148e072fef6c58f84a92
Elias Calagiu <eliasc81@gmail.com>
2020-12-17 20:49:28 -0800

    added rows, text areas, and save buttons; though they are not pretty

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// Create empty array to hold hours variable
+
+var finalHour = "";
+
 // Create variable which holds current date information with moment.js
 var date = moment().format('LLLL'); 
 
@@ -7,7 +11,37 @@ $("date").text((moment()));
 // Append date to header so that it shows up below the text we already have 
 $("header").append(date);
 
+// Get stored todos from local storage
 
-var mondayRow = $("<tr>")
-mondayRow.text("Monday");
-$(".table").append(mondayRow);
+
+
+// // Create an array that contains a string for the hours from 9am - 5pm
+// var hours = ["9am","10am","11am","12pm","1pm","2pm","3pm","4pm","5pm"]
+
+// // Loop through our array and create timeblock divs
+
+// for(var i=0; i < hours.length; i++){
+    
+// }
+
+
+// create table for 
+// var mondayRow = $("<tr>")
+// mondayRow.text("Monday");
+// $(".table").append(mondayRow);
+
+// var tuesdayRow = $("<tr>")
+// tuesdayRow.text("Tuesday");
+// $(".table").append(tuesdayRow);
+
+// var wednesdayRow = $("<tr>")
+// wednesdayRow.text("Wednesday");
+// $(".table").append(wednesdayRow);
+
+// var thursdayRow = $("<tr>")
+// thursdayRow.text("Thursday");
+// $(".table").append(thursdayRow);
+
+// var fridayRow = $("<tr>")
+// fridayRow.text("Friday");
+// $(".table").append(fridayRow);
