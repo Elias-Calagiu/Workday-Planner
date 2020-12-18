12be214f0c7a20b456b2782ff2586402ceabbed6
Elias Calagiu <eliasc81@gmail.com>
2020-12-17 19:21:56 -0800

    added some comments and created a README file

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+// Create variable which holds current date information with moment.js
 var date = moment().format('LLLL'); 
+
+// Add text to date variable
 $("date").text((moment()));
+
+// Append date to header so that it shows up below the text we already have 
 $("header").append(date);
 
-var months = ["January", "Februar"]
+
+var mondayRow = $("<tr>")
+mondayRow.text("Monday");
+$(".table").append(mondayRow);
